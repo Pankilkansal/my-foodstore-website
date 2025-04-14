5371450f1e9c3e456a0b351c3220316548978317
Pankil Kansal <pankilkansal3@gmail.com>
2025-04-14 16:12:59 -0400

    Updated app.js with new functionality

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const pages = require("./routes/pages");
 
+// MongoDB connection (make sure DB name has no spaces!)
+mongoose.connect('mongodb://127.0.0.1:27017/my_awesome_food_store', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("Error connecting to MongoDB: ", err));
 
+// Set EJS as the view engine
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views"); // Correct view path
+app.set("views", __dirname + "/views");
+
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files
 app.use(express.static("public"));
+
+// Use your routes
 app.use("/", pages);
-app.use(express.static("public")); // Serves static files like images, CSS, etc.
 
-
+// Start server
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
